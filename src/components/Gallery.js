dcab94eed70779c4aea656321df65a35c2f2fd22
KamilSzus <kamil-szustak1@wp.pl>
2022-04-04 21:56:00 +0200

    ...

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,53 +1,46 @@
 import React from 'react'
 import "./Gallery.css"
-import {Box, Card, CardActionArea, CardContent, Grid, makeStyles, Typography} from "@material-ui/core";
+import {Box, Card, CardActionArea, CardContent, Grid, Typography} from "@material-ui/core";
 
 function GalleryImage(props) {
-    return ((
-            <div>
-                <Grid container columns={{xs: 4, md: 12}}>
-                    <Card className="card" variant="outlined" square>
-                        <CardActionArea
-                            href={''}
-                            target="_blank">
-                            <Box
-                                p={1}
-                                style={{
-                                    height: 250,
-                                    textAlign: 'center'
-                                }}>
-                                <img
-                                    src={props.src}
-                                    className="media"
-                                    alt={'ff'}/>
-                            </Box>
-                            <CardContent className="content">
-                                <Typography
-                                    variant="body1"
-                                    color="textPrimary"
-                                    gutterBottom
-                                    style={{
-                                        overflow: 'hidden',
-                                        textOverflow: 'ellipsis',
-                                        display: '-webkit-box',
-                                        WebkitLineClamp: '2',
-                                        WebkitBoxOrient: 'vertical'
-                                    }}>
-                                    {"item.title"}
-                                </Typography>
-                                <Typography variant="subtitle2" color="textSecondary">
-                                    {'item.price.currency'} {'item.price.value'}
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            </div>
+    return (props.myArray.map((object) => (
+
+            <Card className="card" variant="outlined">
+                <CardActionArea
+                    href={''}
+                    target="_blank">
+                    <Box
+                        p={1}
+                        style={{
+                            height: 250,
+                            textAlign: 'center'
+                        }}>
+                        <img
+                            src={object.src}
+                            className="media"
+                            alt={'coo'}/>
+                    </Box>
+                    <CardContent className="content">
+                        <Typography
+                            variant="body1"
+                            color="textPrimary"
+                            gutterBottom
+                            style={{
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                display: '-webkit-box',
+                                WebkitLineClamp: '2',
+                                WebkitBoxOrient: 'vertical'
+                            }}>
+                            {"item.title"}
+                        </Typography>
+                        <Typography variant="subtitle2" color="textSecondary">
+                            {'item.price.currency'} {'item.price.value'}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
         )
-    )
+    ))
 }
 export default GalleryImage;
-
-//props.items &&
-//props.items.length > 0 &&
-//props.items.map((item) =>
--- src/components/Pages/Items.js
+++ src/components/Pages/Items.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import 'react-slideshow-image/dist/styles.css'
 import GalleryImage from "../Gallery";
-function Items() {
-    return(
-        <>
-                <GalleryImage src='images/img-9.jpg' title="1 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
-                <GalleryImage src='images/img-9.jpg' title="2 Lorem ipsum dolor sit amet" />
+import {Grid} from "@material-ui/core";
 
+const myArray = [
+    {
+        number: 9,
+        title: 'Users',
+        src: 'images/img-9.jpg',
+    },
+    {
+        number: 8,
+        title: 'Users',
+        src: 'images/img-8.jpg',
+    },
+    {
+        number: 7,
+        title: 'Users',
+        src: 'images/img-7.jpg',
+    },
+    {
+        number: 6,
+        title: 'Users',
+        src: 'images/img-6.jpg',
+    },
+    {
+        number: 9,
+        title: 'Users',
+        src: 'images/img-9.jpg',
+    },
+    {
+        number: 8,
+        title: 'Users',
+        src: 'images/img-8.jpg',
+    },
+    {
+        number: 7,
+        title: 'Users',
+        src: 'images/img-7.jpg',
+    },
+    {
+        number: 6,
+        title: 'Users',
+        src: 'images/img-6.jpg',
+    },
+]
+
+function Items() {
+    return (
+        <>
+            <div>
+                <Grid container columns={{xs: 4, md: 12}}>
+                    <GalleryImage myArray={myArray}/>
+                </Grid>
+            </div>
         </>
     )
 }
